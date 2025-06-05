6c5f46b6b44970595a8bf960e4d5d38b42d568c3
Alex <alex.drobotov94@gmail.com>
2025-06-05 17:25:47 +0300

    chore(deps): init dependencies

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function App() {
+  return <Title>HI</Title>;
+}
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+export default App;
